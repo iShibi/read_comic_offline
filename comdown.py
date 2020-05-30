9041137b73473a86b4acc34259e602eeb9b51db9
Shubham Parihar <51517103+shibi391@users.noreply.github.com>
2020-05-30 17:25:52 +0530

    Added new powerful features
    This update brings url mode, search mode, ability to choose comic quality, and firefox support to read_comic_offline

--- comdown.py
+++ comdown.py
@@ -1,83 +1,119 @@
-# make sure you download chromedriver.exe first and put that in the same directory where this file is.
+# This program supports both Chrome and Firefox in headless as well as in head mode. readcomiconline contains a lot of ads,
+# that's why this program installs an adblock extension for Firefox (in headless as well as in head mode) everytime you run it.
+# Chrome does not support extensions in headless mode so if you want to use chrome with extensions you will have to run it in head mode after
+# setting the path to your chrome profile down below. The profile path will help selenium to open your chrome browser with all your settings and
+# extensions in place. I could do the same with Firefox instead of installing adblock everytime but Firefox has an issue where it opens your
+# profile but your installed extensions don't work even though they are enabled. So, both Chrome and Firefox have some issues but still I'll advice
+# you to use Firefox mode because it's easy and less work. For Chrome, extensions are not available in headless mode and even in head mode you
+# will have to do some work to make it open the browser with already installed extensions.
 
-# importing required modules
+
+# These two lines make the terminal clean, they are optional.
+from os import system
+system('cls')
+
+browser_option = input("Choose a browser (chrome/firefox): ")
+browser_mode = input("Choose a mode (head/headless): ")
+
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
+if browser_option == 'chrome':
+    from selenium.webdriver.chrome.options import Options
+elif browser_option == 'firefox':
+    from selenium.webdriver.firefox.options import Options
+
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import urllib.request
 import patoolib
 import os
 
-path = r'path to the folder where you want to save the downloaded pages of the comic'
-# making an empty list 'files' which will contain the downloaded pages as elements.
-files = []
-# selenium will wait till the specified time limit before throwing an error if it does not find the element you are trying to get. Increase this if you have a slow internet connection.
-timeout = 15
-# initializing a variable 'downloading' to true, the while loop will run till this variable is true. The download funtion will make it false if selenium does not find a comic page in given time limit of 'timeout'
-downloading = True
 
-# program will ask user these three questions on execution.
-comic_name = input("Which comic you want to download: ")                            # name of the comic user want to download
-year = input(f"In which year '{comic_name}' was first published: ")                 # year of publication of issue number 1 of the given comic
-issue_number = input(f"Which issue of '{comic_name}' you want to download: ")       # comic issue number which user wants to download
-
-# link to the homepage of readcomiconline website
-url = "https://readcomiconline.to/"
-
-# changing selenium's default settings
-chrome_options = Options()
-profile = r"put your chrome browser profile here to keep the browser settings and extensions"
-chrome_options.add_argument(f"--user-data-dir={profile}")                           # without this selenium will open a brand new chrome browser without your settings and extensions
-chrome_options.add_argument('log-level=3')                                          # this will stop selenium from printing unnecessary information in the terminal
-browser = webdriver.Chrome(options=chrome_options)                                  # initializing the browser object
-browser.get(url)                                                                    # this will open the homepage of readcomiconline website
-
-# function which will download the comic pages
-def download(page_number):
+def download(page):
     try:
-        img_xpath = f'//*[@id="divImage"]/p[{str(page_number)}]/img'
+        img_xpath = f'//*[@id="divImage"]/p[{str(page)}]/img'
         img = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, img_xpath)))
         link = img.get_attribute('src')
-        urllib.request.urlretrieve(link, f"pages/{page_number}.jpg")
+        urllib.request.urlretrieve(link, f"pages/{page}.jpg")
     except:
         global downloading
         downloading = False
 
-# finding the search bar on the homepage
-search_bar = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.ID, "keyword")))
-# entering the comic name in the search bar
-search_bar.send_keys(comic_name + " " + year)
 
-# finding the search button on the homepage
-search = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.ID, "imgSearch")))
-# clicking on the search button
-search.click()
+path = r'PATH\TO\The\FOLDER\WHERE\YOU\WANT\TO\SAVE\YOUR\COMIC\PAGES'
+files = []
+timeout = 20
+i = 1
+downloading = True
 
-# finding the first result, which is the comic the user wants to download
-comic_xpath = r'//*[@id="leftside"]/div/div[2]/div/table/tbody/tr[3]/td[1]/a'
-comic = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, comic_xpath)))
-# clicking on the first result
-comic.click()
+quality_set = input("\nChoose quality of the comic (high/low): ")
+comic_url = input("Enter url of the comic (Enter 's' to open search mode): ")
 
-# finding the required issue from the list of all published issues
-issue = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, f"{issue_number}")))
-# clicking on the required issue
-issue.click()
+options = Options()
 
-page_number = 1
+if browser_mode == 'headless':
+    options.headless = True
+
+if browser_option == 'chrome':
+    profile = r"C:\Users\Username\AppData\Local\Google\Chrome\User Data"        # search 'chrome://version' to find your chrome profile path. Remove the stuff after 'User Data'
+    options.add_argument(f"--user-data-dir={profile}")
+    browser = webdriver.Chrome(options = options, executable_path=r"PATH\TO\chromedriver.exe")
+    #options.add_argument('log-level=3')
+    #options.add_experimental_option('excludeSwitches', ['enable-logging'])
+elif browser_option == 'firefox':
+    browser = webdriver.Firefox(options = options, executable_path=r"PATH\TO\geckodriver.exe")
+    browser.install_addon(r"PATH\TO\uBlock0@raymondhill.net.xpi")          # this will install an adblock extension in firefox
+
+if comic_url == 's':
+    comic_name = input("\nWhich comic you want to download: ")
+    year = int(input(f"In which year '{comic_name}' was first published (Enter '1' if it's a oneshot): "))
+    if year != 1:
+        issue_number = input(f"Which issue of '{comic_name}' you want to download: ")
+    url = "https://readcomiconline.to/"
+
+    browser.get(url)
+
+    search_bar = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.ID, "keyword")))
+    search_bar.send_keys(comic_name)
+
+    search = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, r'//*[@id="imgSearch"]')))
+    search.click()
+
+    if year == 1:
+        comic = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, r"(//*[@class='listing']//a)[1] | (//*[@class='list']//a)[1]")))
+        comic.click()
+    else:
+        comic = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, f"{year}")))
+        comic.click()        
+
+    if year != 1:        
+        top_issue = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, r"(//*[@class='listing']//a)[1] | (//*[@class='list']//a)[1]")))
+        top_issue_name = top_issue.text
+        top_issue_number = int(top_issue_name.partition("#")[-1])
+        required_issue = top_issue_number - (int(issue_number) - 1)
+        issue = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, f"(//*[@class='listing']//a)[{required_issue}] | (//*[@class='list']//a)[{required_issue}]")))
+        issue.click()
+else:
+    browser.get(comic_url)
+
+if quality_set == 'high':
+    quality = Select(WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.ID, "selectQuality"))))
+    quality.select_by_value("hq")
+
+reading_type = Select(WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, r'//*[@id="selectReadType"]'))))
+reading_type.select_by_value("1")
+
 print("Downloading...")
-# calling the download funtion inside loop till all the pages are downloaded
-while downloading:                      # download is true by default and will remain true till all the pages are downloaded
-    download(page_number)               # calling the download funtion with page number as an argument
-    page_number += 1                    # increasing page number by 1 with each iteration
+while downloading:
+    download(i)
+    i += 1
 
 
 print("Download Complete")
-# storing all the downloaded pages in the list 'files'
 for r, d, f in os.walk(path):
     for file in f:
         if '.jpg' in file:
@@ -85,16 +121,17 @@ for r, d, f in os.walk(path):
 
 
 print("Converting images into .cbr format...")
-# converting the list 'files' into a tuple 'images' because that is what patool supports
 images = tuple(files)
-# using patool to convert all the elements inside the tuple 'images' into a .cbr file
-patoolib.create_archive(f"#{issue_number}.cbr", images, verbosity=-1)
+if comic_url == 's':
+    patoolib.create_archive(f"#{issue_number}.cbr", images, verbosity=-1)
+else:
+    patoolib.create_archive("comic.cbr", images, verbosity=-1)
 
 
-# this is optional. If you do not want to delete the downloaded images after making the .cbr file, just remove this part
 print("Deleting the images...")
 for f in files:
     os.remove(f)
 
 
-print(f"Issue #{issue_number} has been downloaded successfully!")
+print(f"Comic has been downloaded successfully!")
+browser.close()
